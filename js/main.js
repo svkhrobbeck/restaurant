a3c248051e8a72afe9159813657bbd1280d82dec
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-02-21 15:58:21 +0500

    add menu section and make menu section responsive

--- js/main.js
+++ js/main.js
@@ -23,3 +23,34 @@ function siteNavOutsideClick(evt) {
 
   document.querySelector("[data-sitenav-toggler]").classList.remove("show");
 }
+
+// Swiper
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: false,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
